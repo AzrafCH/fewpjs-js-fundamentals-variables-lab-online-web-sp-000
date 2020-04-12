e220afef936e0eb1a9cc2a62146c3ad4f2a7b996
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-12 03:30:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const companyName = 'Scuber';
 
 let mostProfitableNeighborhood = 'Chelsea';
 
-mostProfitableNeighborhood = 'Upper West Side';
+mostProfitableNeighborhood == 'Upper West Side';
 
 let companyCeo = 'Susan Smith';
